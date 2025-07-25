b637c2a34c21a660b20b07d573d2a54acfe93a2e
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2025-07-25 11:57:05 +0200

    Change license

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
-# Copyright (c) 2020 Leiden University Medical Center
-# Copyright (c) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
-# 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022
-# Python Software Foundation; All Rights Reserved
-
-# This file is part of python-isal which is distributed under the
-# PYTHON SOFTWARE FOUNDATION LICENSE VERSION 2.
-
-import os
-import sys
+# Copyright (C) 2025 Leiden University Medical Center
+# This file is part of nfs-usage-utils
+#
+# nfs-usage-utils is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# nfs-usage-utils is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with nfs-usage-utils.  If not, see <https://www.gnu.org/licenses/
 
 from setuptools import Extension, setup
 
--- src/nfs_usage_utils/nfscrawler.py
+++ src/nfs_usage_utils/nfscrawler.py
@@ -1,22 +1,18 @@
-# Copyright (c) 2025 Leiden University Medical Center
+# Copyright (C) 2025 Leiden University Medical Center
+# This file is part of nfs-usage-utils
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
+# nfs-usage-utils is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
 #
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
+# nfs-usage-utils is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+# You should have received a copy of the GNU Affero General Public License
+# along with nfs-usage-utils.  If not, see <https://www.gnu.org/licenses/
 """
 Simple utility to list all the files present on the NFS filesystem. As of yet
 no predicates are implemented.
