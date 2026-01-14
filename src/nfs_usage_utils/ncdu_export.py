1d4817258fd2e5c455a925f5d6fe09555477c514
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2026-01-14 11:01:18 +0100

    Implement data compression

--- src/nfs_usage_utils/ncdu_export.py
+++ src/nfs_usage_utils/ncdu_export.py
@@ -64,7 +64,7 @@ class Info(typing.NamedTuple):
 
     @classmethod
     def from_bytes(cls, buffer: bytes):
-        path, data = buffer.split(b"\x00")
+        path, data = buffer.split(b"\x00", maxsplit=1)
         asize, dsize, dev, ino, nlink, filetype = struct.unpack("<QQQQIB", data)
         return cls(
             path = path.decode("utf-8"),
@@ -84,7 +84,7 @@ class Info(typing.NamedTuple):
                 f"Path and name basename do not match. {entry.path} | {entry.name}"
             )
         if entry.is_file():
-            tp = FILE,
+            tp = FILE
         elif entry.is_dir():
             tp = DIR
         else:
@@ -115,22 +115,11 @@ class Info(typing.NamedTuple):
             answer["notreg"] = True
         return answer
 
+    def is_dir(self):
+        return self.filetype == DIR
 
-def NFSDirEntry_to_info_block(entry: _nfs.NFSDirEntry, parent_dev: int = 0) -> Dict[str, Any]:
-    answer = {
-        "name": entry.name,
-        "asize": entry.st_size,
-        "dsize": entry.st_blocks * entry.st_blksize,
-    }
-    if entry.st_dev != parent_dev:
-        answer["dev"] = entry.st_dev
-    if entry.st_nlink > 1:
-        answer["ino"] = entry.st_ino
-        answer["nlink"] = entry.st_nlink
-        answer["hlnkc"] = True
-    if not (entry.is_dir() or entry.is_file()):
-        answer["notreg"] = True
-    return answer
+    def is_file(self):
+        return self.filetype == FILE
 
 
 def main():
@@ -142,12 +131,18 @@ def main():
     url, prefix = nfs_url_and_prefix_from_args(args)
     with _nfs.NFSMount(url, hash_size = args.max_requests // 10) as mount:
         crawl = crawlnfs(mount, max_requests=args.max_requests)
+
+        compressed_entries = [
+            Info.from_nfs_dir_entry(entry).to_bytes() for entry in crawl
+        ]
         # Set the path separator to \x00 so it comes before all other characters.
         # This ensures that after sorting directories always come before the
         # respective files.
-        entries: List[_nfs.NFSDirEntry] = sorted(
-            crawl, key=lambda x: x.path.replace("/", "\x00"))
-    if len(entries) < 1:
+        # Since the compressed entries start with the path there is no need to
+        # unpack them
+        compressed_entries.sort(key=lambda x: x.replace(b"/" , b"\x00"))
+
+    if len(compressed_entries) < 1:
         return
 
     with open(args.out, "wt") as out:
@@ -157,16 +152,17 @@ def main():
 
         out.write(f"[{major_version}, {minor_version}, {json.dumps(metadata)},\n")
 
-        entry_iter: Iterator[_nfs.NFSDirEntry] = iter(entries)
-        first_entry: _nfs.NFSDirEntry = next(entry_iter)
+        compressed_entry_iter: Iterator[bytes] = iter(compressed_entries)
+        first_entry: Info = Info.from_bytes(next(compressed_entry_iter))
         assert (first_entry.is_dir())
-        first_entry_info = NFSDirEntry_to_info_block(first_entry, -1)
+        first_entry_info = first_entry.to_json(parent_dev=-1)
         # The top level entry should have the full path according to the spec.
         first_entry_info["name"] = prefix
         out.write("[")
         out.write(json.dumps(first_entry_info))
         current_dirs = [first_entry]
-        for entry in entry_iter:  # type: _nfs.NFSDirEntry
+        for compressed_entry in compressed_entry_iter:  # type: bytes
+            entry: Info = Info.from_bytes(compressed_entry)
             current_dir = current_dirs[-1]
             while os.path.dirname(entry.path) != current_dir.path:
 
@@ -177,12 +173,10 @@ def main():
                 out.write(",\n[")
                 current_dirs.append(entry)
                 current_dir = entry
-                out.write(json.dumps(NFSDirEntry_to_info_block(
-                    entry, parent_dev=current_dir.st_dev)))
+                out.write(json.dumps(entry.to_json(parent_dev=current_dir.dev)))
             else:
                 out.write(",\n")
-                out.write(json.dumps(NFSDirEntry_to_info_block(
-                    entry, parent_dev=current_dir.st_dev)))
+                out.write(json.dumps(entry.to_json(parent_dev=current_dir.dev)))
         while len(current_dirs) > 0:
             current_dirs.pop()
             out.write("]")  # Finish open directories
